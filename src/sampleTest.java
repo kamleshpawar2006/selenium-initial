5ff2d26cf6785b9be6f7fcdfc05d66b85933ec9e
kamleshpawar2006 <kamleshpawar2006@gmail.com>
2021-11-09 00:25:56 +0530

    github login test

--- src/sampleTest.java
+++ src/sampleTest.java
@@ -13,25 +13,36 @@ public class sampleTest {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","D:/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.freelancer.com/login");
+		driver.get("https://github.com/login");
 
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		
 		try {
-            wait.until(presenceOfElementLocated(By.className("CheckboxLabel")));
-            System.out.println("found");
+            wait.until(presenceOfElementLocated(By.id("login_field")));
+            System.out.println("login field found");
         } catch(Exception e) {
-        	System.out.println("not found");
+        	System.out.println("login field not found");
             driver.quit();
         }
 
-		WebElement username = driver.findElement(By.xpath("//input[@type='email']"));
-		WebElement password = driver.findElement(By.xpath("//input[@type='password']"));
+		WebElement username = driver.findElement(By.id("login_field"));
+		WebElement password = driver.findElement(By.id("password"));
 
-		username.sendKeys("akmlesh@gmail.com");
-		password.sendKeys("kamlesh");
+		username.sendKeys("useremail@gmail.com");
+		password.sendKeys("1234556");
 
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
+		
+		try {
+            wait.until(presenceOfElementLocated(By.id("js-flash-container")));
+            System.out.println("found error message");
+        } catch(Exception e) {
+        	System.out.println("error message not found");
+            driver.quit();
+        }
+		
+//		testNg
+//		Assert.assertEquals(expectedUrl,actualUrl);
 		 
 	}
 }
